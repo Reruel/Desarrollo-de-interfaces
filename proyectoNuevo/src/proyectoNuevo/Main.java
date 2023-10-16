5f17eabce3fd6b201e03656c863e13b932331485
Reruel <reyesrubioelena@gmail.com>
2023-10-16 12:12:54 +0200

    a

--- proyectoNuevo/src/proyectoNuevo/Main.java
+++ proyectoNuevo/src/proyectoNuevo/Main.java
@@ -1,10 +1,11 @@
 package proyectoNuevo;
-
+import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String nombre= "Reyes";
+		JOptionPane.showMessasgeDialog(null,"Bienvenido"+nombre);
 	}
 
 }
