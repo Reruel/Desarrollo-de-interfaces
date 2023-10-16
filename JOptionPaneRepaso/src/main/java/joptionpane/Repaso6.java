baf55780fd9badf1f042d294e1ea8a8a6620745e
Reruel <115066257+Reruel@users.noreply.github.com>
2023-10-16 13:54:51 +0200

    a

--- JOptionPaneRepaso/src/main/java/joptionpane/Repaso6.java
+++ JOptionPaneRepaso/src/main/java/joptionpane/Repaso6.java
@@ -1,20 +1,21 @@
 package joptionpane;
 
 import javax.swing.JOptionPane;
-import javax.swing.UIManager;  //Esto es para poner en español el texto de los botones
+import javax.swing.UIManager; //Esto es para poner en español el texto de los botones
 
 public class Repaso6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		UIManager.put("OptionPane.yesButtonText",  "Sí");
-		UIManager.put("OptionPane.noButtonText",  "No");
-		UIManager.put("OptionPane.cancelButtonText",  "Cancelar");
-		
-		String presentacion=JOptionPane.showInputDialog(null, "¿Cómo te llamas?", "Preséntate", JOptionPane.INFORMATION_MESSAGE);
+
+		UIManager.put("OptionPane.yesButtonText", "Sí");
+		UIManager.put("OptionPane.noButtonText", "No");
+		UIManager.put("OptionPane.cancelButtonText", "Cancelar");
+
+		String presentacion = JOptionPane.showInputDialog(null, "¿Cómo te llamas?", "Preséntate",
+				JOptionPane.INFORMATION_MESSAGE);
 		JOptionPane.showMessageDialog(null, "Hola " + presentacion);
-		
+
 		int codigo = JOptionPane.showConfirmDialog(null, "¿Quieres un euro para una buena causa?", "Donación",
 				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE);
 
